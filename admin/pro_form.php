ca9aca72dda02e6d13a540915e3b4be7ac45a4e6
Hendrik Eka <muhesyah@gmail.com>
2018-02-03 20:06:39 +0700

    upload multiple file

--- admin/pro_form.php
+++ admin/pro_form.php
@@ -9,24 +9,27 @@ if(isset($_POST['submit'])){ //['submit'] merupakan name dari button di form tam
 	$peralatan	= $_POST['peralatan'];
 	$hasil	= $_POST['hasil'];
 	$teknisi	= $_POST['teknisi'];
-	
-	foreach ($_FILES['foto']['name'] as $key => $foto) {
-		// Rename nama fotonya dengan menambahkan tanggal dan jam upload
-		$fotobaru = $foto;
-		// Set path folder tempat menyimpan fotonya
-		move_uploaded_file($_FILES['foto']['tmp_name'][$key], 'images/' . $fotobaru);
+	$count = count($_FILES['foto']);
+	$filename = array();
+	for($i=0;$i<$count;$i++) {
+		// Rename nama fotonya
+		$fotobaru = $_FILES['foto']['name'][$i];
        	$path = "images/".$fotobaru;
-       	$sql	= "INSERT INTO log SET id_log='', tgl='$tgl', jam='$jam', pemeliharaan='$pemeliharaan', nama_peralatan='$peralatan', hasil='$hasil', teknisi='$teknisi', dokumentasi='$foto' ";
+		// Set path folder tempat menyimpan fotonya
+		move_uploaded_file($_FILES['foto']['tmp_name'][$i], $path);
+		$filename = $fotobaru;
+	}
+	$foto = json_encode($filename);
+	$sql	= "INSERT INTO log SET id_log='', tgl='$tgl', jam='$jam', pemeliharaan='$pemeliharaan', nama_peralatan='$peralatan', hasil='$hasil', teknisi='$teknisi', dokumentasi='$foto' ";
 
-       	$query	= mysqli_query($conn,$sql); // Eksekusi/ Jalankan query dari variabel $query
-	  if($sql){ // Cek jika proses simpan ke database sukses atau tidak
-	    // Jika Sukses, Lakukan :
-	    header("location: index.php"); // Redirect ke halaman index.php
-	  }else{
-	    // Jika Gagal, Lakukan :
-	    echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
-	    echo "<br><a href='form.php'>Kembali Ke Form</a>";
-	  }
+	$query	= mysqli_query($conn,$sql); // Eksekusi/ Jalankan query dari variabel $query
+	if($sql){ // Cek jika proses simpan ke database sukses atau tidak
+		// Jika Sukses, Lakukan :
+		header("location: index.php"); // Redirect ke halaman index.php
+	}else{
+		// Jika Gagal, Lakukan :
+		echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
+		echo "<br><a href='form.php'>Kembali Ke Form</a>";
 	}
 }
 	//$tmp = $_FILES['foto']['tmp_name'];
